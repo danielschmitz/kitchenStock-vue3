fa69c11687374a3e3c695864391eecb538744e7d
Daniel Schmitz <danieljfa@gmail.com>
2023-08-21 13:07:03 -0300

    ✨ Signup form

---
+++ src/views/SignUp.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import TitleBar from '@/components/TitleBar.vue'
+</script>
+
+<template>
+  <TitleBar title="SignUp" sub-title="Crie uma conta para poder logar"></TitleBar>
+
+  <div class="m-5">
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="field">
+          <label class="label">Phone</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="field is-grouped is-grouped-right has-background-light p-3">
+    <p class="control">
+      <a class="button is-primary"> Submit </a>
+    </p>
+    <p class="control">
+      <a class="button is-light"> Cancel </a>
+    </p>
+  </div>
+  
+</template>
+
+<style scoped></style>
