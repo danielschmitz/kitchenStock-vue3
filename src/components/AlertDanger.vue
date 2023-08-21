91389d9b5b332c65a542332c9546db65038abf81
Daniel Schmitz <danieljfa@gmail.com>
2023-08-21 12:24:59 -0300

    ✨ Login information

---
+++ src/components/AlertDanger.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <article class="message is-danger">
+    <div class="message-body"><slot/></div>
+  </article>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-
 <script setup lang="ts">
-  import TitleBar from '../components/TitleBar.vue'
+import TitleBar from '../components/TitleBar.vue'
+import AlertDanger from '../components/AlertDanger.vue'
+import { auth } from '../auth'
 </script>
 
-
 <template>
-  <TitleBar title="Bem vindo" subtitle="bem vindo ao Vue!"/>
+  <TitleBar title="Bem vindo" sub-title="Bem vindo a uma aplicação de exemplo com Vue" />
+
+  <div class="block pt-2">
+    <div v-if="!auth.isLogged()">
+      <AlertDanger>Você não está logado. Clique em login/signup</AlertDanger>
+    </div>
+    <div v-else>
+      login..
+    </div>
+  </div>
 </template>
