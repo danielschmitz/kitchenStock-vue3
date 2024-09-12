53f4aab1e23654ebd9d36d579e33b616f73a676b
Daniel Schmitz <danieljfa@gmail.com>
2024-09-12 10:07:27 -0300

    ✨ Novo filtro

---
+++ src/components/ActionButtons.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="field is-grouped is-grouped-centered pt-5">
+    <RouterLink to="/stock/add">
+      <a class="button is-primary mr-2">Adicionar Item ao Estoque</a>
+    </RouterLink>
+    <RouterLink to="/products/Create">
+      <a class="button is-primary">Criar Novo Produto</a>
+    </RouterLink>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import TitleBar from '../components/TitleBar.vue'
 import AlertDanger from '../components/AlertDanger.vue'
+import ActionButtons from '../components/ActionButtons.vue'
 import { auth } from '../auth'
 import { onMounted, ref } from 'vue'
 import type Stock from '@/dto/Stock'
@@ -9,9 +10,14 @@ import Spinner from '@/components/Spinner.vue'
 import utils from '@/utils'
 import { useRoute } from 'vue-router'
 import router from '@/router'
+import type Category from '@/dto/Category'
+import CategoryService from '@/services/CategoryService'
 
 const stockList = ref<Stock[]>()
+const categories = ref<Category[]>()
+const filteredStockList = ref<Stock[]>()
 const loading = ref<boolean>(false)
+const selectedCategory = ref<number | null>(null)
 
 const checkDate = (date) => {
   if (utils.isDateLessToday(date)) return 'red'
@@ -23,9 +29,21 @@ const changeStock = (id) => {
   router.push({ path: `/stock/edit/${id}` })
 }
 
+const filterByCategory = (category: number | null) => {
+  console.log(category)
+  selectedCategory.value = category
+  if (category) {
+    filteredStockList.value = stockList.value?.filter(stock => stock.product?.categoryId === category) || []
+  } else {
+    filteredStockList.value = stockList.value || []
+  }
+}
+
 onMounted(async () => {
   loading.value = true
   stockList.value = await StockService.getAll()
+  categories.value = await CategoryService.getAll()
+  filteredStockList.value = stockList.value || []
   loading.value = false
 })
 </script>
@@ -37,22 +55,29 @@ onMounted(async () => {
         <AlertDanger>Você não está logado. Clique em login/signup</AlertDanger>
       </div>
       <div v-else>
-        <div class="field is-grouped is-grouped-centered">
-          <RouterLink to="/stock/add"
-            ><a class="button is-primary mr-2">Add Item to Stock</a></RouterLink
-          >
-          <RouterLink to="/products/Create"
-            ><a class="button is-primary">Create a New Product</a></RouterLink
-          >
-        </div>
+        <ActionButtons />
         <br />
         <Spinner v-if="loading"></Spinner>
+
         <div v-else class="ml-5 mr-5">
+          <div class="buttons is-centered mb-4">
+            <button class="button is-link" :class="{ 'is-primary': selectedCategory === null }" @click="filterByCategory(null)">
+              Todos
+            </button>
+            <button v-for="category in categories" 
+                    :key="category" 
+                    class="button" 
+                    :class="{ 'is-primary': selectedCategory === category.name }"
+                    @click="filterByCategory(category.id)">
+              {{ category.name }}
+            </button>
+          </div>
+
           <table class="table is-fullwidth is-bordered is-striped is-narrow is-hoverable">
             <tbody>
-              <tr v-for="stock in stockList" :key="stock.id" @click="changeStock(stock.id)">
+              <tr v-for="stock in filteredStockList" :key="stock.id" @click="changeStock(stock.id)">
                 <td class="quantity">{{ stock.quantity }}</td>
-                <td>{{ stock.product?.name }} ({{ stock.product?.supplier }}) {{ utils.isDateLessOneMonth(stock.expires) ? 'yellow' : 'black'  }} </td>
+                <td>{{ stock.product?.name }} ({{ stock.product?.supplier }}) </td>
                 <td style="width: 55px" 
                     :class="checkDate(stock.expires)">{{ $filters.formatDate(stock.expires) }}</td>
               </tr>
@@ -75,12 +100,12 @@ tr {
 }
 
 .red {
-  color: brown;
+  color: brown!important;
   font-weight: bold;
 }
 
 .yellow {
-  color: darkgoldenrod;
+  color: darkgoldenrod!important;
   font-weight: bold;
 }
 
